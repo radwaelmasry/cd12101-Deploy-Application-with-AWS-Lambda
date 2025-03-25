fb38b9a3cb426c86cfbe9b8b910b893989726542
Radwa Elmasry <relmasry@ejada.com>
2025-03-25 15:13:31 +0200

    Changes

--- starter/client/src/config.ts
+++ starter/client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'drcy8tho78'
+const apiId = 'dohjsbu24a'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-inrw113fveqldu4e.us.auth0.com',           // Auth0 domain
-  clientId: 'l8ILfXKKqkSOQTG0ILlAGegeNjukYUlF',  // Auth0 client id
+  domain: 'dev-i0r6xxpp5tsetahl.us.auth0.com',           // Auth0 domain
+  clientId: 'fjXw42zF2lTpxJXZ9LUgVbULy0PqugUJ',  // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 };
